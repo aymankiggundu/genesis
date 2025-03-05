d83ddde901d241cdf048dd7d0c9df485d1b387f6
Kiggundu Sulaiman <kiggundusulaiman332@gmail.com>
2025-03-05 15:31:43 +0300

    genesis

--- genesis/ContentView.swift
+++ genesis/ContentView.swift
@@ -9,80 +9,44 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
+        TabView {
+            CameraView()
+                .tabItem {
+                    Label("Camera", systemImage: "camera")
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
+            ChatView()
+                .tabItem {
+                    Label("Chat", systemImage: "message")
                 }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
+            StoriesView()
+                .tabItem {
+                    Label("Stories", systemImage: "book")
                 }
-            }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
         }
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
+struct CameraView: View {
+    var body: some View {
+        Text("Camera View")
+    }
+}
+
+struct ChatView: View {
+    var body: some View {
+        Text("Chat View")
+    }
+}
+
+struct StoriesView: View {
+    var body: some View {
+        Text("Stories View")
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
---
+++ genesis/StoriesView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct StoriesView: View {
+    var body: some View {
+        Text("Stories View")
+    }
+}
+
+struct StoriesView_Previews: PreviewProvider {
+    static var previews: some View {
+        StoriesView()
+    }
+}
